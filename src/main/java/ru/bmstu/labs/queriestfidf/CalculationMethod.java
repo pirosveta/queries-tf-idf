74683ff8facb0d6ac8307d392fc0196cdd34e93f
pirosveta <svetiklanka.2000@yandex.ru>
2023-04-06 19:04:42 +0300

    Calculation methods extract into separate Enum class

---
+++ src/main/java/ru/bmstu/labs/queriestfidf/CalculationMethod.java
@@ -0,0 +1,8 @@
+package ru.bmstu.labs.queriestfidf;
+
+public enum CalculationMethod {
+    VECTOR_MODEL_TF,
+    VECTOR_MODEL_LOG_TF,
+    LANGUAGE_MODEL_LAMBDA_0_5,
+    LANGUAGE_MODEL_LAMBDA_0_9
+}
