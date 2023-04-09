677826f0c22c44e196bd26efb8dc21715db1ff6c
pirosveta <svetiklanka.2000@yandex.ru>
2023-04-09 08:43:19 +0300

    NDCG calculation added

--- src/main/java/ru/bmstu/labs/queriestfidf/QueriesTfIdfApplication.java
+++ src/main/java/ru/bmstu/labs/queriestfidf/QueriesTfIdfApplication.java
@@ -3,30 +3,33 @@ package ru.bmstu.labs.queriestfidf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import ru.bmstu.labs.queriestfidf.model.Document;
-import ru.bmstu.labs.queriestfidf.model.DocumentWord;
-import ru.bmstu.labs.queriestfidf.model.Query;
-import ru.bmstu.labs.queriestfidf.model.Word;
-import ru.bmstu.labs.queriestfidf.service.DocumentService;
-import ru.bmstu.labs.queriestfidf.service.DocumentWordService;
-import ru.bmstu.labs.queriestfidf.service.QueryService;
-import ru.bmstu.labs.queriestfidf.service.WordService;
+import ru.bmstu.labs.queriestfidf.model.*;
+import ru.bmstu.labs.queriestfidf.service.*;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
 
+import static ru.bmstu.labs.queriestfidf.CalculationMethod.LANGUAGE_MODEL_LAMBDA_0_5;
+import static ru.bmstu.labs.queriestfidf.CalculationMethod.LANGUAGE_MODEL_LAMBDA_0_9;
+
 @SpringBootApplication
 public class QueriesTfIdfApplication {
+
+    private static final double LAMBDA_0_5 = 0.5, LAMBDA_0_9 = 0.9, TOP_POSITIONS = 10;
+    private static final String OUTPUT_EXTENSION = ".txt";
 
     private static DocumentService documentService;
     private static WordService wordService;
     private static DocumentWordService documentWordService;
     private static QueryService queryService;
+    private static QueryDocumentService queryDocumentService;
 
     private static Lemmatizer lemmatizer;
     private static CalculationMethod calculationMethod;
+
+    private static Integer totalCollectionWords;
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = SpringApplication.run(ru.bmstu.labs.queriestfidf.QueriesTfIdfApplication.class, args);
@@ -38,6 +41,7 @@ public class QueriesTfIdfApplication {
         wordService = applicationContext.getBean(WordService.class);
         documentWordService = applicationContext.getBean(DocumentWordService.class);
         queryService = applicationContext.getBean(QueryService.class);
+        queryDocumentService = applicationContext.getBean(QueryDocumentService.class);
 
         /**
          * Additional beans initialization
@@ -50,7 +54,7 @@ public class QueriesTfIdfApplication {
         initializeDatabase();
 
         /**
-         * Relevance calculation for all methods
+         * Relevance and NDCG calculation for all methods
          */
         calculate();
     }
@@ -62,6 +66,14 @@ public class QueriesTfIdfApplication {
     }
 
     private static void calculate() {
+        HashMap<CalculationMethod, HashMap<String, Double>> ndcgs = new HashMap<>();
+        totalCollectionWords = 0;
+
+        List<Document> documents = documentService.getEntities();
+        for (Document document : documents) {
+            totalCollectionWords += document.getLength();
+        }
+
         Arrays.stream(CalculationMethod.values()).forEach(m -> {
             StringBuilder prefix = new StringBuilder();
             calculationMethod = m;
@@ -81,7 +93,43 @@ public class QueriesTfIdfApplication {
             printRelevanceToFile(triggerRelevance, prefix + "trigger-relevance");
             printRelevanceToFile(knotRelevance, prefix + "knot-relevance");
             printRelevanceToFile(tsrRelevance, prefix + "tsr-relevance");
+
+            ndcgs.put(m, new HashMap<>() {{
+                put("trigger", ndcgCalculate(1, triggerRelevance));
+            }});
+            ndcgs.get(m).put("knot", ndcgCalculate(2, knotRelevance));
+            ndcgs.get(m).put("tsr", ndcgCalculate(3, tsrRelevance));
         });
+
+        printNdcgsToFile(ndcgs);
+    }
+
+    private static double ndcgCalculate(Integer queryId, List<Map.Entry<Document, Double>> relevance) {
+        double idcg = 0;
+        double dcg = 0;
+
+        List<QueryDocument> queryDocuments = queryDocumentService.getByQueryId(queryId);
+        queryDocuments.sort(Comparator.comparing(QueryDocument::getRelevance));
+
+        int currentPosition = 1;
+        for (QueryDocument queryDocument : queryDocuments) {
+            idcg += queryDocument.getRelevance() / Math.log(currentPosition + 1);
+            currentPosition++;
+        }
+
+        currentPosition = 1;
+        while (currentPosition <= TOP_POSITIONS) {
+            Integer documentId = relevance.get(currentPosition - 1).getKey().getId();
+            Optional<QueryDocument> queryDocumentOptional = queryDocumentService.getByQueryIdAndDocumentId(queryId, documentId);
+
+            if (queryDocumentOptional.isPresent()) {
+                dcg += queryDocumentOptional.get().getRelevance() / Math.log(currentPosition + 1);
+            }
+
+            currentPosition++;
+        }
+
+        return dcg / idcg;
     }
 
     private static List<Map.Entry<Document, Double>> getRelevanceForQuery(Integer id) {
@@ -94,11 +142,16 @@ public class QueriesTfIdfApplication {
         HashMap<Document, Double> relevances = new HashMap<>();
 
         for (Document document : documents) {
-            double relevanceValue = 0;
+            double relevanceValue = switch (calculationMethod) {
+                case VECTOR_MODEL_TF, VECTOR_MODEL_LOG_TF -> 0;
+                case LANGUAGE_MODEL_LAMBDA_0_5, LANGUAGE_MODEL_LAMBDA_0_9 -> 1;
+            };
+
             for (Map.Entry<Integer, Double> queryValue : vector.entrySet()) {
                 if (queryValue.getKey() > 0) {
                     Word word = wordService.getById(queryValue.getKey());
                     Optional<DocumentWord> documentWord = documentWordService.getByDocumentAndWord(document, word);
+
                     if (documentWord.isPresent()) {
                         switch (calculationMethod) {
                             case VECTOR_MODEL_TF ->
@@ -107,7 +160,31 @@ public class QueriesTfIdfApplication {
                             case VECTOR_MODEL_LOG_TF ->
                                     relevanceValue += (Math.log(documentWord.get().getFrequency() + 1) * word.getIdf().doubleValue())
                                             * Math.log(queryValue.getValue() + 1);  // Using of log(tf+1)
+                            case LANGUAGE_MODEL_LAMBDA_0_5 ->
+                                    relevanceValue *= (LAMBDA_0_5 * documentWord.get().getFrequency() / document.getLength())
+                                            + ((1 - LAMBDA_0_5) * queryValue.getValue() / totalCollectionWords);
+                            case LANGUAGE_MODEL_LAMBDA_0_9 ->
+                                    relevanceValue *= (LAMBDA_0_9 * documentWord.get().getFrequency() / document.getLength())
+                                            + ((1 - LAMBDA_0_9) * queryValue.getValue() / totalCollectionWords);
                         }
+                    } else {
+                        switch (calculationMethod) {
+                            case LANGUAGE_MODEL_LAMBDA_0_5 ->
+                                    relevanceValue *= (1 - LAMBDA_0_5) * queryValue.getValue() / totalCollectionWords;
+                            case LANGUAGE_MODEL_LAMBDA_0_9 ->
+                                    relevanceValue *= (1 - LAMBDA_0_9) * queryValue.getValue() / totalCollectionWords;
+                        }
+
+                    }
+                } else if (calculationMethod == LANGUAGE_MODEL_LAMBDA_0_5
+                        || calculationMethod == LANGUAGE_MODEL_LAMBDA_0_9) {
+                    switch (calculationMethod) {
+                        case LANGUAGE_MODEL_LAMBDA_0_5 ->
+                                relevanceValue *= (LAMBDA_0_5 * 1 / (document.getLength() + 1))
+                                        + ((1 - LAMBDA_0_5) * queryValue.getValue() / (totalCollectionWords + 1));
+                        case LANGUAGE_MODEL_LAMBDA_0_9 ->
+                                relevanceValue *= (LAMBDA_0_9 * 1 / (document.getLength() + 1))
+                                        + ((1 - LAMBDA_0_9) * queryValue.getValue() / (totalCollectionWords + 1));
                     }
                 }
             }
@@ -150,17 +227,36 @@ public class QueriesTfIdfApplication {
             }
         }
 
-        double length = 0;
-        for (Map.Entry<Integer, Integer> entry : frequencyDictionary.entrySet()) {
-            switch (calculationMethod) {
-                case VECTOR_MODEL_TF -> length += entry.getValue() * entry.getValue();
-                case VECTOR_MODEL_LOG_TF -> length += Math.log(entry.getValue() + 1) * Math.log(entry.getValue() + 1);
+        if (calculationMethod == CalculationMethod.VECTOR_MODEL_TF
+                || calculationMethod == CalculationMethod.VECTOR_MODEL_LOG_TF) {
+            double length = 0;
+            for (Map.Entry<Integer, Integer> entry : frequencyDictionary.entrySet()) {
+                switch (calculationMethod) {
+                    case VECTOR_MODEL_TF -> length += entry.getValue() * entry.getValue();
+                    case VECTOR_MODEL_LOG_TF ->
+                            length += Math.log(entry.getValue() + 1) * Math.log(entry.getValue() + 1);
+                }
             }
-        }
-        length = Math.sqrt(length);
+            length = Math.sqrt(length);
 
-        for (Map.Entry<Integer, Integer> entry : frequencyDictionary.entrySet()) {
-            vector.put(entry.getKey(), entry.getValue().doubleValue() / length);
+            for (Map.Entry<Integer, Integer> entry : frequencyDictionary.entrySet()) {
+                vector.put(entry.getKey(), entry.getValue().doubleValue() / length);
+            }
+        } else if (calculationMethod == LANGUAGE_MODEL_LAMBDA_0_5
+                || calculationMethod == LANGUAGE_MODEL_LAMBDA_0_9) {
+            for (Map.Entry<Integer, Integer> entry : frequencyDictionary.entrySet()) {
+                int totalWordFrequency = 1;
+
+                if (entry.getKey() > 0) {
+                    List<DocumentWord> documentWords = documentWordService.getByWord(wordService.getById(entry.getKey()));
+
+                    for (DocumentWord documentWord : documentWords) {
+                        totalWordFrequency += documentWord.getFrequency();
+                    }
+                }
+
+                vector.put(entry.getKey(), (double) totalWordFrequency);
+            }
         }
 
         return vector;
@@ -168,12 +264,32 @@ public class QueriesTfIdfApplication {
 
     private static void printRelevanceToFile(List<Map.Entry<Document, Double>> relevances, String fileName) {
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName + OUTPUT_EXTENSION));
             for (Map.Entry<Document, Double> relevance : relevances) {
                 writer.append(relevance.getValue().toString())
                         .append("\t\t")
                         .append(relevance.getKey().getValue())
                         .append("\n");
+            }
+            writer.close();
+        } catch (IOException ignored) {
+        }
+    }
+
+    private static void printNdcgsToFile(HashMap<CalculationMethod, HashMap<String, Double>> ndcgs) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("ndcg" + OUTPUT_EXTENSION));
+            for (Map.Entry<CalculationMethod, HashMap<String, Double>> ndcg : ndcgs.entrySet()) {
+                writer.append(ndcg.getKey().name())
+                        .append("\n");
+
+                for (Map.Entry<String, Double> queryNdcg : ndcg.getValue().entrySet()) {
+                    writer.append(queryNdcg.getKey())
+                            .append("\t\t")
+                            .append(queryNdcg.getValue().toString())
+                            .append("\n");
+                }
+                writer.append("\n");
             }
             writer.close();
         } catch (IOException ignored) {
